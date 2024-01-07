a277d7667da00e3db5c7a21906157934571e59d0
Chris Gallegos <fermion@email.com>
2024-01-06 17:11:14 -0800

    Initial Commit
    Lava lamp shader for Unity Game Engine

---
+++ LavaLamp.shader
@@ -0,0 +1,141 @@
+﻿Shader "Unlit/LavaLamp"
+{
+    Properties
+    {
+        _LampSize("Lamp Size", Vector) = (1.0, 1.0, 0.0, 0.0)
+        _TopPercent("Top Percent", Float) = 1.0
+        _Threshold("Threshold", Float) = 1.0
+        _AntiAliasing("Anti-Aliasing", Float) = 0.1
+
+        [Header(Lighting)]
+        _FresnelColor("FresnelColor", Color) = (1, 1, 1, 1)
+        _FresnelDistance("Fresnel Distance", Float) = 0.1
+        _FresnelPower("Fresnel Power", Float) = 2
+        _Specular("Specular", Float) = 2
+        _SpecularPower("Specular Power", Float) = 2
+        _SpecularOpacity("Specular Opacity", Float) = 0.5
+
+        [Header(Colors)]
+        _DefaultColor("Default Color", Color) = (1, 1, 1, 1)
+        _TopColor("Top Color", Color) = (0, 0, 1, 1)
+        _BottomColor("Bottom Color", Color) = (1, 0, 1, 1)
+        _GradientStrength("Gradient Strength", Float) = 1
+
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Transparent" "Queue"="Transparent" }
+        LOD 100
+
+        ZWrite Off
+        Blend SrcAlpha OneMinusSrcAlpha
+
+        Pass
+        {
+            CGPROGRAM
+
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct v2f
+            {
+                float2 uv : TEXCOORD0;
+                float4 vertex : SV_POSITION;
+            };
+
+            float2 _LampSize;
+
+            float _TopPercent;
+            float _Threshold;
+            float _AntiAliasing;
+
+            // lighting
+            float4 _FresnelColor;
+            float _FresnelDistance;
+            float _FresnelPower;
+            float _Specular;
+            float _SpecularPower;
+            float _SpecularOpacity;
+
+            float _BlobPositions[20];
+            float _BlobWeights[10];
+
+            // colors
+            float4 _DefaultColor;
+            float4 _TopColor;
+            float4 _BottomColor;
+            float _GradientStrength;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv;
+                o.uv.x = o.uv.x - 0.5f;
+
+                return o;
+            }
+
+            float GetValue(float distance, float radius) {
+
+                float x = distance * 0.707f / radius;
+                return x < 0.707f ? 4 * (x * x * x * x - x * x + 0.25f) : 0;
+            }
+
+            fixed4 frag(v2f input) : SV_Target
+            {
+                if (1 - input.uv.y * (1 - _TopPercent) < abs(input.uv.x * 2))
+                    discard;
+
+                float2 coords = input.uv * _LampSize;
+
+                float value = 0;
+                for (int i = 0; i < 10; i++) {
+
+                    float2 displacement = float2(_BlobPositions[2 * i] - coords.x, _BlobPositions[2*i + 1] - coords.y);
+                    value += GetValue(sqrt(dot(displacement, displacement)), _BlobWeights[i]);
+                }
+
+                // expand blob near top
+                float topValue = saturate((1 - input.uv.y * 3) * _Threshold);
+                value += topValue * topValue;
+
+                // expand blob near bottom
+                float bottomValue = saturate((input.uv.y * 3 - 2) * _Threshold);
+                value += bottomValue * bottomValue;
+
+                float2 gradient = float2(ddx(value), ddy(value));
+                float mask = smoothstep(_Threshold - _AntiAliasing, _Threshold + _AntiAliasing, value);
+
+                // fresnel
+                float fresnel = clamp(value - _Threshold, 0, _FresnelDistance) / _FresnelDistance;
+                fresnel = pow(1.01f - fresnel, _FresnelPower);
+
+                // specular
+                float2 specularDirection = float2(0.707, 0.707);
+                float specularSpot = 1 - saturate(length(specularDirection + gradient));
+                float specular = saturate(pow(specularSpot * _Specular / value, _SpecularPower)) * _SpecularOpacity;
+                
+                // diffuse
+                float colorGradient = (0.5f * gradient.y * _GradientStrength + 0.5f);
+
+                _TopColor *= 0.5f * input.uv.y + 0.5f;
+                _BottomColor *= 0.5f * (1 - input.uv.y) + 0.5f;
+
+                float4 diffuse = lerp(_TopColor, _BottomColor, saturate(colorGradient));
+                diffuse = _DefaultColor * (1 - diffuse.a) + diffuse * diffuse.a;
+
+                return mask * (diffuse + fresnel * _FresnelColor * _FresnelColor.a + specular);
+            }
+            ENDCG
+        }
+    }
+}
---
+++ LavaLampController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+[Serializable]
+public struct Blob {
+
+    public float size;
+    public Vector2 position;
+
+    [HideInInspector]
+    public float temperature;
+
+    [HideInInspector]
+    public Vector2 velocity;
+}
+
+public class LavaLampController : MonoBehaviour
+{
+    public Material lavaLampMaterial;
+    public Renderer quad;
+
+    [Header("Bounds")]
+    public Vector2 lampBounds;
+    public float topPercent = 1;
+
+    [Header("Physics")]
+    public float temperatureGradient = 0.5f;
+    public float repulsiveForce = 0.1f;
+    public float viscosity = 2f;
+
+    [Header("Blobs")]
+    public Blob[] blobs;
+
+    protected float[] blobPositions;
+    protected float[] blobWeights;
+
+    protected void OnDrawGizmosSelected() {
+        
+        Vector3 center = quad.transform.position;
+
+        Vector2 extents = 0.5f * lampBounds;
+        Vector3 bottomLeft = center - (Vector3)extents;
+
+        extents.x *= -1;
+        Vector3 bottomRight = center - (Vector3)extents;
+
+        extents.x *= topPercent;
+        Vector3 topLeft = center + (Vector3)extents;
+
+        extents.x *= -1;
+        Vector3 topRight = center + (Vector3)extents;
+
+        Gizmos.DrawLine(bottomLeft, bottomRight);
+        Gizmos.DrawLine(bottomRight, topRight);
+        Gizmos.DrawLine(topRight, topLeft);
+        Gizmos.DrawLine(topLeft, bottomLeft);
+
+        Vector3 origin = center - lampBounds.y * 0.5f * Vector3.up;
+        for (int i = 0; i < blobs.Length; i++) {
+            Gizmos.DrawWireSphere(origin + (Vector3)blobs[i].position, 0.05f);
+        }
+    }
+
+    protected void OnValidate() {
+
+        lavaLampMaterial.SetVector("_LampSize", lampBounds);
+
+        if (Application.isPlaying && blobWeights != null) {
+
+            for (int i = 0; i < blobs.Length; i++) {
+                blobWeights[i] = blobs[i].size;
+            }
+
+            lavaLampMaterial.SetFloatArray("_BlobWeights", blobWeights);
+        }
+    }
+
+    protected void Awake() {
+
+        // set up material
+        quad.material = lavaLampMaterial;
+        lavaLampMaterial.SetFloat("_TopPercent", topPercent);
+        lavaLampMaterial.SetVector("_LampSize", lampBounds);
+
+        blobPositions = new float[blobs.Length * 2];
+        blobWeights = new float[blobs.Length];
+
+        for (int i = 0; i < blobs.Length; i++) {
+            blobWeights[i] = blobs[i].size;
+        }
+
+        lavaLampMaterial.SetFloatArray("_BlobWeights", blobWeights);
+
+        RandomizeBlobs(blobs);
+    }
+
+    protected void FixedUpdate() {
+
+        UpdateBlobs();
+
+        for (int i = 0; i < blobs.Length; i++) {
+            blobPositions[2 * i + 0] = blobs[i].position.x;
+            blobPositions[2 * i + 1] = blobs[i].position.y;
+        }
+
+        lavaLampMaterial.SetFloatArray("_BlobPositions", blobPositions);
+    }
+
+    protected void RandomizeBlobs(Blob[] array) {
+
+        for (int i = 0; i < array.Length; i++) {
+
+            array[i].temperature = Random.Range(-1f, 1f);
+            array[i].velocity = new Vector2(Random.Range(-0.5f, 0.5f), Random.Range(-0.5f, 0.5f));
+        }
+    }
+
+    protected void UpdateBlobs() {
+
+        for (int i = 0; i < blobs.Length; i++) {
+
+            // update temperature
+            float centerHeight = lampBounds.y * 0.5f;
+            float deltaTemp = (centerHeight - blobs[i].position.y) * temperatureGradient * Time.fixedDeltaTime / (lampBounds.y * 0.5f);
+            blobs[i].temperature += Mathf.Clamp(deltaTemp, -temperatureGradient, temperatureGradient);
+
+            // vertical acceleration from temperature
+            float mass = Mathf.Max(0.001f, blobs[i].size);
+            blobs[i].velocity += Vector2.up * blobs[i].temperature * Time.fixedDeltaTime / mass;
+
+            // repulsize force
+            for (int j = 0; j < blobs.Length; j++) {
+
+                if (i != j) {
+
+                    Vector2 displacement = blobs[i].position - blobs[j].position;
+                    float distanceSqr = displacement.sqrMagnitude;
+                    blobs[i].velocity += Time.fixedDeltaTime * repulsiveForce * displacement / (distanceSqr * distanceSqr);
+                }
+            }
+
+            // wall force
+            float verticalPercentage = Mathf.Lerp(1, topPercent, blobs[i].position.y / lampBounds.y);
+            blobs[i].velocity += blobs[i].position.y < 0            ? Time.fixedDeltaTime * Vector2.up    : Vector2.zero;
+            blobs[i].velocity += blobs[i].position.y > lampBounds.y ? Time.fixedDeltaTime * Vector2.down  : Vector2.zero;
+
+            blobs[i].velocity += blobs[i].position.x < -lampBounds.x * 0.5f * verticalPercentage
+                                ? Time.fixedDeltaTime * Vector2.right : Vector2.zero;
+            blobs[i].velocity += blobs[i].position.x > lampBounds.x * 0.5f * verticalPercentage 
+                                ? Time.fixedDeltaTime * Vector2.left : Vector2.zero;
+
+            // update position
+            blobs[i].velocity -= Time.fixedDeltaTime * blobs[i].velocity * viscosity;
+            blobs[i].position += blobs[i].velocity * Time.fixedDeltaTime;
+
+        }
+    }
+}
